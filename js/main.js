0b624f0179d05a30eaaf9f2540189241348fe76f
rzatalibov555 <sethubpro@gmail.com>
2022-10-11 20:05:22 +0400

    events js

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+
+// function goster(){
+//     document.getElementById('satici').innerHTML = "+994557778899"
+// }
+
+
+// function rengini_deyiw(){
+//     document.getElementById('satici').style.color = "red"
+// }
+
+// function rengini_deyiw_yawil(){
+//     document.getElementById('satici').style.color = "green"
+// }
+
+// function baxildi(){
+//     document.getElementById('satici').innerHTML = "Nomreye baxildi!"
+// }
+
+// function revan(){
+//     document.body.style.backgroundColor = "yellow"
+// }
+
+
+function mawin_sec(){
+    var select = document.getElementById('select')
+    var sekil  = document.getElementById('sekil')
+
+    sekil.src = "img/"+select.value+".png"
+
+    // if(select.value == "bmw"){
+    //     sekil.src = "https://www.bmwgroup.com/content/dam/grpw/websites/bmwgroup_com/responsibility/720x720_Nachhaltigkeit.jpg.grp-transform/small/720x720_Nachhaltigkeit.jpg"
+    // }else if(select.value == "Kia"){
+    //     sekil.src = "https://www.kia.com/content/dam/kwcms/ec/es/images/vehicles/kiamodelos/kia_seltos.png"
+    // }
+}
+
+
+
+
+
+
+// function rengi_deyis(e){
+//     e.style.background = "blue"
+// }
+
+// function rengi_qaytar(deyanet){
+//     deyanet.style.background = "green"
+// }
+
+// function rengi_redlewdir(esmira){
+//     esmira.style.border = "5px solid red"
+//     esmira.style.background = "transparent"
+// }
+
+// function divi_sil(e){
+//     e.style.display = "none"
+   
+// }
